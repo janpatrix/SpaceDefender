a18457ebfffa7913094f8c8ce840050c15007679
janpatrix <janpatrix@googlemail.com>
2018-03-12 12:00:59 +0100

    Initial Commit

--- app/src/main/java/com/example/janpatrix/spacedefender/MainActivity.java
+++ app/src/main/java/com/example/janpatrix/spacedefender/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.janpatrix.spacedefender;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button btn_Play = (Button) findViewById(R.id.btn_play);
+        btn_Play.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
+        finish();
     }
 }
